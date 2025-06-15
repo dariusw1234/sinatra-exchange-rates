21f50afdac4c0444072c97ffc9a6a3fb7b39026a
Darius Woods <dariuswoods1919@gmail.com>
2025-06-15 03:30:02 +0000

    created http requests to gather info on excchange rates. compared data in an interactive web page with dynamic routes and embedded code

--- app.rb
+++ app.rb
@@ -1,9 +1,45 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "http"
+require "json"
+
+exchange_api_key = ENV.fetch("EXC_API_KEY")
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @list_url = "https://api.exchangerate.host/list?access_key=#{exchange_api_key}"
+  @currencies = HTTP.get(@list_url).to_s
+  @parsed_currencies = JSON.parse(@currencies)
+  @currencies_hash = @parsed_currencies.fetch("currencies")
+
+  @list_of_currencies = @currencies_hash.keys
+  erb(:currency_pairs)
+end
+
+get("/:from_currency") do
+  @list_url = "https://api.exchangerate.host/list?access_key=#{exchange_api_key}"
+  @currencies = HTTP.get(@list_url).to_s
+  @parsed_currencies = JSON.parse(@currencies)
+  @currencies_hash = @parsed_currencies.fetch("currencies")
+
+  @original_currency = params.fetch("from_currency")
+  @list_of_currencies = @currencies_hash.keys
+  erb(:convert)
+end
+
+get("/:from_currency/:to_currency") do
+  @list_url = "https://api.exchangerate.host/list?access_key=#{exchange_api_key}"
+  @currencies = HTTP.get(@list_url).to_s
+  @parsed_currencies = JSON.parse(@currencies)
+  @currencies_hash = @parsed_currencies.fetch("currencies")
+  
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+  @list_of_currencies = @currencies_hash.keys
+
+  @convert_url = "https://api.exchangerate.host/convert?from=#{@original_currency}&to=#{@destination_currency}&amount=1&access_key=#{exchange_api_key}"
+  @converts = HTTP.get(@convert_url)
+  @parsed_convert = JSON.parse(@converts)
+  @result = @parsed_convert.fetch("result")
+  erb(:convert_to)
 end
